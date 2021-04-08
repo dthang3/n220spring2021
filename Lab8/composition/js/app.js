16e9c2fa7b6b985a2141be80a9b59ae40ef04a69
Daniel C Thang <danthang@iu.edu>
2021-04-08 14:51:48 -0400

    Lab8

---
+++ Lab8/composition/js/app.js
@@ -0,0 +1,9 @@
+let dvBox = document.getElementById("box");
+
+function myFunction(){
+    dvBox.innerHTML=" The Hello my name is KID";
+    dvBox.style.backgroundColor="#66fc03";
+    dvBox.style.height= "200px";
+    dvBox.style.width = "200px";
+    dvBox.style.margin = "20px";
+}

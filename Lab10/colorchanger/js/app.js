d936636d84a89654135cd8f1da8a2359c2fabb2d
Daniel C Thang <danthang@iu.edu>
2021-04-21 19:05:23 -0400

    Lab10

---
+++ Lab10/colorchanger/js/app.js
@@ -0,0 +1,5 @@
+
+function fun(e){
+    var ele = document.getElementById(e.id);
+    ele.style.backgroundColor = ele.dataset.color;
+}
---
+++ Lab10/flashcard/js/app.js
@@ -0,0 +1,6 @@
+function myFunction(clicked) {
+    if (clicked=="Indianapolis")
+    document.getElementById("answer").innerHTML = "It is Right Answer"; 
+    else
+    document.getElementById("answer").innerHTML = "You have Choose Incorrect.True Answer is: Indianapolis"; 
+    }  
